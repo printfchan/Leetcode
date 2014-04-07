080ff616bbbd8096cc7c2b1943be7ae225c33f44
printfchan <printfchan@126.com>
2014-04-07 13:11:55 +0800

    Evaluate Reverse Polish Notation

--- Leetcode/2. Evaluate Reverse Polish Notation/Evaluate Reverse Polish Notation.cpp
+++ Leetcode/2. Evaluate Reverse Polish Notation/Evaluate Reverse Polish Notation.cpp
@@ -1,10 +1,57 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
 using namespace std;
+
+class Solution {
+public:
+	static int evalRPN(vector<string> &tokens) {
+		vector<int> num;
+		vector<string>::iterator it;
+		for (it = tokens.begin(); it != tokens.end(); ++it)
+		{
+			if ((*it)[0] >= '0' && (*it)[0] <= '9' || (*it).length() > 1)
+			{
+				num.push_back(atoi(it->c_str()));
+			}
+			else{
+				int b = num.back();
+				num.pop_back();
+				int a = num.back();
+				num.pop_back();
+				switch ((*it)[0])
+				{
+				case '+':
+					num.push_back(a + b);
+					break;
+				case '-':
+					num.push_back(a - b);
+					break;
+				case '*':
+					num.push_back(a * b);
+					break;
+				case '/':
+					num.push_back(a / b);
+					break;
+				default:
+					break;
+				}
+			}
+			
+		}
+		return num.back();
+	}
+};
 
 int main()
 {
-	int n;
-	cin >> n;
+	vector<string> expression;
+	expression.push_back("3");
+	expression.push_back("-4");
+	expression.push_back("+");
+
+	cout << Solution::evalRPN(expression);
 
 	system("pause");
 	return 0;
