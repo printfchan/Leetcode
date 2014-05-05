75f549e847dc9f9e9b5792af75e38fffbcc814b7
printfchan <printfchan@126.com>
2014-05-05 19:26:32 +0800

    18. Gas Station

--- Leetcode/18. Gas Station/Gas Station.cpp
+++ Leetcode/18. Gas Station/Gas Station.cpp
@@ -18,7 +18,33 @@ using namespace std;
 class Solution {
 public:
 	int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+		if (gas.size() != cost.size() || gas.size() == 0)
+			return -1;
 
+		vector<int> status(gas.size());
+		int sum = 0;
+		for (int i = 0; i < gas.size(); ++i)
+		{
+			status[i] = gas[i] - cost[i];
+			sum += status[i];
+		}
+		if (sum < 0)
+		{
+			return -1;
+		}
+
+		sum = 0;
+		int startIndex = 0;
+		for (int i = 0; i < gas.size(); ++i)
+		{
+			sum += status[i];
+			if (sum < 0)
+			{
+				startIndex = ((i + 1) % gas.size());
+				sum = 0;
+			}
+		}
+		return startIndex;
 	}
 };
 int main()
@@ -26,15 +52,15 @@ int main()
 	vector<int> gas;
 	vector<int> cost;
 
-	gas.push_back(1);
-	gas.push_back(2);
-	gas.push_back(5);
 	gas.push_back(4);
+	/*gas.push_back(2);
+	gas.push_back(5);
+	gas.push_back(4);*/
 
-	cost.push_back(2);
-	cost.push_back(2);
+	cost.push_back(5);
+	/*cost.push_back(2);
 	cost.push_back(4);
-	cost.push_back(4);
+	cost.push_back(4);*/
 
 	Solution solution;
 	
