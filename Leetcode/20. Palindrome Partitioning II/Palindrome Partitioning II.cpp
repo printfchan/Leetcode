94b1f5c3cde002d26d41b82ece040fa04df1c184
printfchan <printfchan@126.com>
2014-05-10 22:34:21 +0800

    add 20,21

---
+++ Leetcode/20. Palindrome Partitioning II/Palindrome Partitioning II.cpp
@@ -0,0 +1,32 @@
+/************************************************************************/
+/* 
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return the minimum cuts needed for a palindrome partitioning of s.
+
+For example, given s = "aab",
+Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 cut.
+*/
+/************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+	int minCut(string s) {
+
+	}
+};
+
+int main()
+{
+	Solution solution;
+
+	string str = "sdjfosjaiog";
+
+	cout << solution.minCut(str);
+
+	system("pause");
+	return 0;
+}
---
+++ Leetcode/21. Palindrome Partitioning/Palindrome Partitioning.cpp
@@ -0,0 +1,39 @@
+/************************************************************************/
+/* 
+given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return all possible palindrome partitioning of s.
+
+For example, given s = "aab",
+Return
+
+[
+	["aa","b"],
+	["a","a","b"]
+]
+*/
+/************************************************************************/
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	vector<vector<string>> partition(string s) {
+
+	}
+};
+
+int main()
+{
+	Solution solution;
+	
+	string str = "sdjfabaaiocc";
+
+	solution.partition(str);
+
+	system("pause");
+	return 0;
+}
